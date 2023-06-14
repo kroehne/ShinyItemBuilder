32d636e9ccc6e2f2c12dfe9d572dcfb77114d884
Ulf Kröhne <ulf@kroehne.de>
2023-06-14 23:58:13 +0200

    Update parse_ib_scoring.R

--- R/parse_ib_scoring.R
+++ R/parse_ib_scoring.R
@@ -13,7 +13,6 @@
 
 parse_ib_scoring <- function(scoringjson){
 
-  scoringjson <- '{"hit.task_1a_correct":false,"hitText.task_1a_correct":"","hitWeighted.task_1a_correct":0,"hitClass.task_1a_correct":"task_1a","hit.task_1a_incorrect":true,"hitText.task_1a_incorrect":"","hitWeighted.task_1a_incorrect":1,"hitClass.task_1a_incorrect":"task_1a","hit.task_1a_missing":false,"hitText.task_1a_missing":"","hitWeighted.task_1a_missing":0,"hitClass.task_1a_missing":"task_1a","hit.task_1a_Verh":false,"hitText.task_1a_Verh":"","hitWeighted.task_1a_Verh":0,"hitClass.task_1a_Verh":"task_1a","hit.task_1a_raw1":true,"hitText.task_1a_raw1":"1","hitWeighted.task_1a_raw1":1,"hitClass.task_1a_raw1":"task_1a_raw1","hit.task_1a_raw2":true,"hitText.task_1a_raw2":"2","hitWeighted.task_1a_raw2":1,"hitClass.task_1a_raw2":"task_1a_raw2","hitsAccumulated":3,"hitsCount":3,"missesAccumulated":0,"missesCount":0,"classHitWeighted.task_1a":1,"classHitWeighted.task_1a_raw2":1,"classHitWeighted.task_1a_raw1":1,"classResult.task_1a":false,"classResult.task_1a_raw2":true,"classResult.task_1a_raw1":true,"classMaxWeighted":1,"classMaxName":"task_1a","totalResult":1,"nbUserInteractions":6,"nbUserInteractionsTotal":6,"firstReactionTime":872,"firstReactionTimeTotal":872,"taskExecutionTime":2488,"taskExecutionTimeTotal":0}'
   scoring <- jsonlite::fromJSON(scoringjson)
 
   a <- list()
